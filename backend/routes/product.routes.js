640b7bdd046e389babd3382395cac1ebbda53d82
moaliyev <mehemmed05.aliyev@gmail.com>
2024-06-16 15:46:15 +0400

    feat: Endpoint added for getting liked products

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createProductController, dislikeProductController, getProductsController, likeProductController } from "../controllers/product.controller.js";
+import { createProductController, dislikeProductController, getLikedProductsController, getProductsController, likeProductController } from "../controllers/product.controller.js";
 
 const router = Router()
 
 router.post("/", createProductController)
-router.get("/", getProductsController) 
+router.get("/", getProductsController)
+router.get("/like", getLikedProductsController)
 router.put("/like/:id", likeProductController)
 router.delete("/like/:id", dislikeProductController)
 
